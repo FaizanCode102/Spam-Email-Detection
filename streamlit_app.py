b4298a93f55472527f067e23052d210e64dc6283
faizan khan <faizank9058@gmail.com>
2024-09-22 23:38:28 +0530

    updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,10 +5,18 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 
-# Download the 'punkt' and 'stopwords' resources
-nltk.download('punkt')
-nltk.download('stopwords')
+# Check and download necessary NLTK resources
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt')
 
+try:
+    nltk.data.find('corpora/stopwords')
+except LookupError:
+    nltk.download('stopwords')
+
+# Initialize stemmer
 ps = PorterStemmer()
 
 def transform_text(text):
@@ -20,42 +28,30 @@ def transform_text(text):
         if i.isalnum():
             y.append(i)
 
-    text = y[:]
-    y.clear()
+    stop_words = set(stopwords.words('english'))
 
-    # Ensure that the stopwords resource is available
-    try:
-        stopwords.words('english')
-    except LookupError:
-        nltk.download('stopwords')
-
-    for i in text:
-        if i not in stopwords.words('english') and i not in string.punctuation:
-            y.append(i)
-
-    text = y[:]
-    y.clear()
-
-    for i in text:
-        y.append(ps.stem(i))
+    for i in y:
+        if i not in stop_words and i not in string.punctuation:
+            y.append(ps.stem(i))
 
     return " ".join(y)
 
+# Load the model and vectorizer
 tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
 model = pickle.load(open('model.pkl', 'rb'))
 
+# Streamlit app layout
 st.title("Spam Email/SMS Detection")
-
 input_sms = st.text_area("Enter the message")
 
 if st.button('Predict'):
-    # 1. preprocess
+    # 1. Preprocess
     transformed_sms = transform_text(input_sms)
-    # 2. vectorize
+    # 2. Vectorize
     vector_input = tfidf.transform([transformed_sms])
-    # 3. predict
+    # 3. Predict
     result = model.predict(vector_input)[0]
-    # 4. Display
+    # 4. Display result
     if result == 1:
         st.header("Spam")
     else:

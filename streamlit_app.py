db2c9f0f2566f239c07419b0b21307e04a6e950f
faizan khan <faizank9058@gmail.com>
2024-09-22 23:40:31 +0530

    updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,19 +5,15 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 
-# Check and download necessary NLTK resources
-try:
-    nltk.data.find('tokenizers/punkt')
-except LookupError:
-    nltk.download('punkt')
+# Download the 'punkt' and 'stopwords' resources
+nltk.download('punkt')
+nltk.download('stopwords')
 
-try:
-    nltk.data.find('corpora/stopwords')
-except LookupError:
-    nltk.download('stopwords')
-
-# Initialize stemmer
 ps = PorterStemmer()
+
+
+
+
 
 def transform_text(text):
     text = text.lower()
@@ -28,30 +24,42 @@ def transform_text(text):
         if i.isalnum():
             y.append(i)
 
-    stop_words = set(stopwords.words('english'))
+    text = y[:]
+    y.clear()
 
-    for i in y:
-        if i not in stop_words and i not in string.punctuation:
-            y.append(ps.stem(i))
+    # Ensure that the stopwords resource is available
+    try:
+        stopwords.words('english')
+    except LookupError:
+        nltk.download('stopwords')
+
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        y.append(ps.stem(i))
 
     return " ".join(y)
 
-# Load the model and vectorizer
 tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
 model = pickle.load(open('model.pkl', 'rb'))
 
-# Streamlit app layout
 st.title("Spam Email/SMS Detection")
+
 input_sms = st.text_area("Enter the message")
 
 if st.button('Predict'):
-    # 1. Preprocess
+    # 1. preprocess
     transformed_sms = transform_text(input_sms)
-    # 2. Vectorize
+    # 2. vectorize
     vector_input = tfidf.transform([transformed_sms])
-    # 3. Predict
+    # 3. predict
     result = model.predict(vector_input)[0]
-    # 4. Display result
+    # 4. Display
     if result == 1:
         st.header("Spam")
     else:
